0e38c6fa76bffe2b11c4c31fa1bed13f57073871
Marko Maksymenko <marko.maksymenko@nure.ua>
2026-04-30 19:58:46 +0300

    Add practice 1 level 1 and 2 tasks

---
+++ practice1_1+2level/practice1.py
@@ -0,0 +1,11 @@
+﻿# Level 1 Task 1
+for number in range(1, 11):
+    print(number)
+
+
+# Level 2 Task 2
+numbers = list(range(1, 21))
+
+for number in numbers:
+    square = number ** 2
+    print(f"Number: {number}, square: {square}")
